9e23a1247af2a8ebd47fb1e8a4a4e3b029c467c3
Kyle Ginnard <kyleginnard@gmail.com>
2024-10-05 03:29:02 -0400

    Change filenames

---
+++ script.js
@@ -0,0 +1,12 @@
+// Add functionality to the window buttons if needed.
+document.querySelector('.close-button').addEventListener('click', () => {
+    alert('Window closed!');
+});
+
+document.querySelector('.minimize-button').addEventListener('click', () => {
+    alert('Window minimized!');
+});
+
+document.querySelector('.maximize-button').addEventListener('click', () => {
+    alert('Window maximized!');
+});
