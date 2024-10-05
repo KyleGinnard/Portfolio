cbd873ff68b65cb7cec7c2612da9e1d4aa2d2866
Kyle Ginnard <kyleginnard@gmail.com>
2024-10-05 09:47:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function toggleTheme() {
-    document.body.classList.toggle('dark');
-}
+// Add functionality to the window buttons if needed.
+document.querySelector('.close-button').addEventListener('click', () => {
+    alert('Window closed!');
+});
+
+document.querySelector('.minimize-button').addEventListener('click', () => {
+    alert('Window minimized!');
+});
+
+document.querySelector('.maximize-button').addEventListener('click', () => {
+    alert('Window maximized!');
+});
