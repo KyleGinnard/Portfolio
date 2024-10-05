169210c3d834a955fb822c3e8d2e09bf1d60beae
Kyle Ginnard <kyleginnard@gmail.com>
2024-10-05 09:21:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const modeSwitch = document.getElementById('mode-switch');
-const body = document.body;
-
-modeSwitch.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    modeSwitch.textContent = body.classList.contains('dark-mode') ? 'Switch to Light Mode' : 'Switch to Dark Mode';
-});
+function toggleTheme() {
+    document.body.classList.toggle('dark');
+}
